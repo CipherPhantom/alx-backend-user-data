dd41381a52c664fbbb17a0936eee9954cd211805
CipherPhantom <oyepitan10@gmail.com>
2024-04-11 14:17:07 +0100

    Correct PII_FIELDS

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,7 @@ import logging
 from typing import List
 
 
-PII_FIELDS = ("name", "email", "ssn", "password", "ip")
-
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(
         fields: List[str],
