9586c2c5710636956b4fce57cc8d0f7e029ca6e0
CipherPhantom <oyepitan10@gmail.com>
2024-04-16 10:50:48 +0100

    Task 3
    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Authentication Module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Manages the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns None"""
+        return None
