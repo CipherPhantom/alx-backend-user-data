33b54eff06d7786ca076e629e3da3d745dbb02dd
CipherPhantom <oyepitan10@gmail.com>
2024-04-16 13:45:33 +0100

    Task 10
    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,10 @@
 
 """ Basic Authentication Module
 """
-from api.v1.auth.auth import Auth
 import base64
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -71,3 +73,28 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """Creates the User instance based on his email and password.
+        Returns:
+            - None if user_email is None or not a string
+            - None if user_pwd is None or not a string
+            - None if your database (file) doesn’t contain any User
+            instance with email equal to user_email
+            - None if user_pwd is not the password of the User instance found
+            - The User instance otherwise
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({"email": user_email})
+        if len(users) == 0:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
