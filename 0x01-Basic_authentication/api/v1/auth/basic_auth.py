20dda167ed8f80f5d7bccb25151c7a196891b15d
CipherPhantom <oyepitan10@gmail.com>
2024-04-16 12:51:19 +0100

    Task 7
    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,27 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Performs Basic Authentication on the API"""
-    pass
+
+    def __init__(self) -> None:
+        """Initializes the class"""
+        super().__init__()
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization
+            header for a Basic Authentication
+        Return:
+            - None if authorization_header is None
+            - None if authorization_header is not a string
+            - None if authorization_header doesn’t start by Basic
+                (with a space at the end)
+            - The value after Basic (after the space) otherwise
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
