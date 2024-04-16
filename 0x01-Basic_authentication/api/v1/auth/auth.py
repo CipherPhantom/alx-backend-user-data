bad0ca9908d04288cb9ec76e07de3441665607c1
CipherPhantom <oyepitan10@gmail.com>
2024-04-16 14:52:59 +0100

    Task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,6 +25,9 @@ class Auth:
             path += '/'
         if path in excluded_paths:
             return False
+        for ex_path in excluded_paths:
+            if ex_path.endswith("*") and path.startswith(ex_path[:-1]):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
