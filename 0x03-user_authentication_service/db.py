a7461103b0e6f203f6f9bea7923c338c19ff961c
CipherPhantom <oyepitan10@gmail.com>
2024-04-23 10:28:32 +0100

    Improve find_user_by function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 from typing import Dict
@@ -35,22 +36,27 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database
         Return:
-            id of the new user
+            - The new user
         """
         try:
             user = User(email=email, hashed_password=hashed_password)
             self._session.add(user)
             self._session.commit()
+            return user
         except Exception:
-            self._session.rollback()
-            user = None
-        return user
+            pass
 
     def find_user_by(self, **kwargs: Dict) -> User:
         """Returns the first row found in the users table as
         filtered by the method’s input arguments
         """
-        user = self._session.query(User).filter_by(**kwargs).first()
+        query = self._session.query(User)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+            query = query.filter(getattr(User, key) == value)
+
+        user = query.first()
         if user is None:
             raise NoResultFound
         return user
